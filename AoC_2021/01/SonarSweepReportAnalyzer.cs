041666fa59978430d9d767d3cfa50610b1353e2d
Marcel Gasser <gasser.marcel@gmail.com>
2021-12-05 07:28:27 +0100

    first test in place

---
+++ AoC_2021/01/SonarSweepReportAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AoC_2021._01
+{
+    public class SonarSweepReportAnalyzer
+    {
+        public SonarSweepReportAnalyzer(IEnumerable<int> measurements)
+        {
+            
+        }
+
+        public int GetNumberOfDepthMeasurementIncreases()
+        {
+            var result = 0;
+
+
+            return result;
+        }
+    }
+}
